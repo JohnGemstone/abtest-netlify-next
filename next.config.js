fab6553560d6e92ddc8f2ccedaa9cdf684ca06a3
Jim Johnston <james.johnston.uk@gmail.com>
2022-02-07 14:36:22 +0000

    testing next rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/product-landing',
+        destination: '/dynamic-routes/product-1',
+      },
+    ]
+  },
 }
